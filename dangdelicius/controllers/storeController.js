9b50940f6d43ee7285c2c0d0196e35c57626c102
Miguel <lowreybadboy@gmail.com>
2017-06-10 17:47:57 +0200

    anado getStoreController para listar mis tiendas

--- dangdelicius/controllers/storeController.js
+++ dangdelicius/controllers/storeController.js
@@ -12,6 +12,10 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
     //Cuando se haya salvado con exito se ejecuta el codigo de a continuacion que es el flash y la redireccion
-    req.flash('sucess', `Succesfully created ${store.name}`)
+    req.flash('success', `Succesfully created ${store.name}`);
     res.redirect(`/store/${store.slug}`);
+}
+exports.getStores = async(req, res) => {
+    const stores = await(Store.find())
+    res.render('stores', { title: 'Stores', stores});
 }
---
+++ dangdelicius/routes/index-mio.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController')
+
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
