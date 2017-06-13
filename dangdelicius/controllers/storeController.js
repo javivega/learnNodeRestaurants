7b78ff8d1d1d4cc2c355956252101aaa72bb6035
Miguel <lowreybadboy@gmail.com>
2017-06-13 15:34:50 +0200

    anado multer para gestionar la subida y anado el campo al form

--- dangdelicius/controllers/storeController.js
+++ dangdelicius/controllers/storeController.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
+//multer manejara la subida de los archivos
+const multer = require('multer');
+//en las opciones le decimos donde tiene que almacenar el archivo y luego con el metodo comprobamos que 
+//sea del tipo imagen y si es asi que prosiga con la subida.
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next){
+        const isPhoto = file.mimetype.startsWith('/image');
+        if(isPhoto){
+            //con este le decimos que si el tipo de archivo es una foto prosiga con la subida
+            next(null, true);
+        } else {
+            next({message: 'No es un tipo de archivo permitido'}, false);
+        }
+    }
+};
+
+
+
 exports.homePage = (req, res) => {
     res.render('index');
 }
@@ -8,6 +27,9 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
     res.render('editStore', { name: 'miguel' });
 }
+
+//podemos manejar varios archivos en la subida pero con single le decimos que solo el campo photo
+exports.upload = multer(multerOptions).single('photo')
 
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
@@ -26,6 +48,7 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async(req, res) => {
+    req.body.location.type = 'Point';
     //el metodo findoneandupdate coge tres parametros que son en ordenL la query, los datos, y opciones
     //el new true hace que se retorne una nueva tienda en lugar de la vieja
     //el runvalidators fuerza al modelo a comprobar los campos requeridos en el modelo, tambien en la actualizacion
