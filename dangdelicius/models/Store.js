8132070192bfcd554ef011e7860141ea4f7aee81
Miguel <lowreybadboy@gmail.com>
2017-06-18 15:08:24 +0200

    genero un slug distinto para tiendas que se llamen igual en el presave

--- dangdelicius/models/Store.js
+++ dangdelicius/models/Store.js
@@ -43,12 +43,22 @@ const storeSchema = new mongoose.Schema({
 
 //Antes de salvar la tienda introducido, haz lo siguiente coger el numbre y aplicarle la funcion slug que defini en lso helpers. Y cuando acaba de salva con el next();
 //solo aplicamos el slug cuando el nombre ha sido modificado si no saltamos.
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function (next) {
     if (!this.isModified('name')) {
         next(); //pasa al siguiente
         return; //acaba la funcion
     }
     this.slug = slug(this.name);
+    //Lo primero sera encontrar las tiendas que coincidan con la regular expresion que escriba
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    //hacemos una busqueda de todas las tiendas que empiecen por los datos introducidos
+    const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+    //si hay mas de uno entonces la length sera mayor que 0 y entonces al slug le añado la length mas uno, para generar
+    //el nuevo slug
+    if(storesWithSlug.length){
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
+
     next();
 })
 
